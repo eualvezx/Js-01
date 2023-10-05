e9aacce4e5ac5283608d37016ad94671c276160b
eualvezx <brunoalvesgame2@gmail.com>
2023-10-04 21:22:01 -0300

    04/10

---
+++ macro.js
@@ -0,0 +1,6 @@
+var n1 = Number(window.prompt('Informe o primeiro número: '))
+var n2 = Number(window.prompt('Informe o segundo número: '))
+var soma = n1 + n2
+
+window.alert('A soma entre ${n1} e ${n2} é ${soma}')
+
